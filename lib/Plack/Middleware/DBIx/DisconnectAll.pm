37df8fb461c6186be578df5e2aa3d9714e627b32
Masahiro Nagano <kazeburo@gmail.com>
2012-12-12 13:01:36 +0900

    initial import

---
+++ lib/Plack/Middleware/DBIx/DisconnectAll.pm
@@ -0,0 +1,41 @@
+package Plack::Middleware::DBIx::DisconnectAll;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Plack::Middleware::DBIx::DisconnectAll - ...
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::DBIx::DisconnectAll;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::DBIx::DisconnectAll is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Nagano
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
