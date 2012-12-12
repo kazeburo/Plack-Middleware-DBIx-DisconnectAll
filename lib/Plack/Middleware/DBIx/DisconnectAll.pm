7cb8cb7ce6749ba673f6958406db6b71ef7145c4
Masahiro Nagano <kazeburo@gmail.com>
2012-12-12 13:17:17 +0900

    disconnectall

--- lib/Plack/Middleware/DBIx/DisconnectAll.pm
+++ lib/Plack/Middleware/DBIx/DisconnectAll.pm
@@ -1,10 +1,27 @@
 package Plack::Middleware::DBIx::DisconnectAll;
+
 use strict;
 use warnings;
 use 5.008005;
+use DBIx::DisconnectAll;
+
 our $VERSION = '0.01';
 
-
+sub call {
+    my ( $self, $env) = @_;
+    my $res = $self->app->($env);
+    Plack::Util::response_cb($res, sub {
+        my $res = shift;
+        return sub {
+            my $chunk = shift;
+            if ( ! defined $chunk ) {
+                dbi_disconnect_all();
+                return;
+            }
+            return $chunk;
+        };
+    });
+}
 
 1;
 __END__
@@ -13,23 +30,34 @@ __END__
 
 =head1 NAME
 
-Plack::Middleware::DBIx::DisconnectAll - ...
+Plack::Middleware::DBIx::DisconnectAll - Disconnect all database connection at end of request
 
 =head1 SYNOPSIS
 
   use Plack::Middleware::DBIx::DisconnectAll;
 
+  use Plack::Builder;
+  
+  builder {
+      enable "DBIx::DisconnectAll";
+      $app
+  };
+
+
 =head1 DESCRIPTION
 
-Plack::Middleware::DBIx::DisconnectAll is
+Plack::Middleware::DBIx::DisconnectAll calls DBIx::DisconnectAll at end of request
+and disconnects all database connections.
 
-B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+This modules is useful for freeing resources.
 
 =head1 AUTHOR
 
 Masahiro Nagano E<lt>kazeburo@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<DBIx::DisconnectAll>
 
 =head1 LICENSE
 
